1d17c17a54cc5757e62e68bbc1bc539ffc9ae9fd
omedia23 <syedhasan.cse@gmail.com>
2022-10-28 18:02:18 +0600

    add article list bloc

---
+++ lib/bloc/article_list_bloc.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+
+import 'package:article_finder/bloc/bloc.dart';
+import 'package:article_finder/data/article.dart';
+import 'package:article_finder/data/rw_client.dart';
+
+class ArticleListBloc implements Bloc {
+  final _client = RWClient();
+  final _searchQueryController = StreamController<String?>();
+  Sink<String?> get searchQuery => _searchQueryController.sink;
+  late Stream<List<Article>?> articleStream;
+
+  ArticleListBloc() {
+    articleStream = _searchQueryController.stream
+        .asyncMap((query) => _client.fetchArticles(query));
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+  }
+}
